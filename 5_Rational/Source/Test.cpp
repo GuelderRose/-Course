d644874e89fc7cd113c5873674fc02a3e98250a4
GuelderRose <52867372+GuelderRose@users.noreply.github.com>
2019-10-05 16:27:59 +0300

    Add files via upload

--- 5_Rational/Source/Test.cpp
+++ 5_Rational/Source/Test.cpp
@@ -1,30 +1,5 @@
 #include "Rational.h"
 
-#define CATCH_CONFIG_MAIN
-#include "C:/Users/Asus/Desktop/catch.hpp" 
-
-TEST_CASE("Tests for TestParse", "[classic]")
-{
-    REQUIRE(TestParse("{3/0}") == false);
-    REQUIRE(TestParse("{3/1}") == true);
-    REQUIRE(TestParse("{0/1}") == true);
-    REQUIRE(TestParse("{3,1}") == false);
-    REQUIRE(TestParse("r") == false);
-}
-TEST_CASE("Tests for operators", "[classic]")
-{
-    Rational z(1, 2);
-    Rational f(1, 2);
-    Rational sum(1, 1);
-    Rational s = z + f;
-    CHECK((sum == s) == true);
-    z = 3;
-    f = { 2, 1 };
-    Rational res(3, 2);
-    Rational r = z / f;
-    CHECK(((z / f) == res)==true);
-}
-/*
 int main() {
     using namespace std;
     Rational z;
@@ -40,4 +15,4 @@ int main() {
     system("pause");
     return(0);
 }
-*/
+
