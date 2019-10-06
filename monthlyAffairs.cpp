50ac21a276ae11968afbb51f2e5b8211e0eb0850
GuelderRose <52867372+GuelderRose@users.noreply.github.com>
2019-10-07 02:58:55 +0300

    Add files via upload

---
+++ monthlyAffairs.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <exception>
+
+// https://www.coursera.org/learn/c-plus-plus-white/programming/gLemQ/iezhiemiesiachnyie-diela
+/*У каждого из нас есть ежемесячные дела,
+каждое из которых нужно выполнять в конкретный день каждого месяца
+оплата счетов за электричество, абонентская плата за связь и пр.
+Вам нужно реализовать работу со списком таких дел, а именно, обработку следующих операций:
+
+ADD i s
+Добавить дело с названием s в день i.
+
+NEXT
+Закончить текущий месяц и начать новый. Если новый месяц имеет больше дней, чем текущий,
+добавленные дни изначально не будут содержать дел. Если же в новом месяце меньше дней,
+дела со всех удаляемых дней необходимо будет переместить на последний день нового месяца.
+
+Обратите внимание, что количество команд этого типа может превышать 11.
+
+DUMP i
+Вывести все дела в день i.
+
+Изначально текущим месяцем считается январь. Количества дней в месяцах соответствуют
+Григорианскому календарю с той лишь разницей, что в феврале всегда 28 дней.
+
+Формат ввода
+Сначала число операций Q, затем описания операций.
+
+Названия дел s уникальны и состоят только из латинских букв, цифр и символов подчёркивания.
+Номера дней i являются целыми числами и нумеруются от 1 до размера текущего месяца.
+
+Формат вывода
+Для каждой операции типа DUMP в отдельной строке выведите количество дел в соответствующий день,
+а затем их названия, разделяя их пробелом. Порядок вывода дел в рамках каждой операции значения не имеет.*/
+int main() {
+    std::vector<std::vector<std::string>> Days(31);
+    std::vector<int> DaysInMonth{ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    int number_of_operations = 0;
+    int next_month = 0;
+    std::cout << "Enter the number of commands : ";
+    std::cin >> number_of_operations;
+    std::vector<std::vector<std::string>> Сommands(number_of_operations);
+    std::string command, day, deal;
+    std::cout << "Enter commands: " << std::endl;
+    for (int i = 0; i < number_of_operations; i++) {
+        std::cin >> command;
+        if (command == "ADD" || command == "DUMP" || command == "NEXT") {
+            if (command == "ADD") {
+                std::cin >> day >> deal;
+                Сommands[i].push_back(command);
+                Сommands[i].push_back(day);
+                Сommands[i].push_back(deal);
+            }
+            if (command == "DUMP") {
+                std::cin >> day;
+                Сommands[i].push_back(command);
+                Сommands[i].push_back(day);
+            }
+            if (command == "NEXT") {
+                Сommands[i].push_back(command);
+            }
+        } else {
+            Сommands[i].push_back("Wrong command");
+        }
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < number_of_operations; i++) {
+        try {
+            if (Сommands[i][0] == "ADD") {
+                if (std::stoi(Сommands[i][1]) > DaysInMonth[next_month]) {
+                    throw std::runtime_error("There is no such number in this month");
+                } else {
+                    Days[stoi(Сommands[i][1]) - 1].push_back(Сommands[i][2]);
+                }
+            }
+        } catch (std::exception& ex) {
+            std::cout << ex.what() << std::endl;
+        }
+        if (Сommands[i][0] == "NEXT") {
+            next_month++;
+            if (next_month == 12) {
+                next_month = 1;
+            }
+            if (DaysInMonth[next_month] > DaysInMonth[next_month - 1]) {
+                for (int j = DaysInMonth[next_month - 1]; j < DaysInMonth[next_month] + 1; j++) {
+                    std::vector<std::string> oneDay;
+                    Days.push_back(oneDay);
+                }
+            }
+            if (DaysInMonth[next_month] < DaysInMonth[next_month - 1]) {
+                std::vector<std::vector<std::string>> Days1(DaysInMonth[next_month]);
+                for (int j = 0; j < Days1.size(); j++) {
+                    Days1[j] = Days[j];
+                }
+                for (int j = 0; j < DaysInMonth[next_month - 1] - DaysInMonth[next_month]; j++) {
+                    if (Days[DaysInMonth[next_month] + j].size() != 0) {
+                        Days1[Days1.size() - 1].insert(end(Days1[Days1.size() - 1]), begin(Days[DaysInMonth[next_month] + j]), end(Days[DaysInMonth[next_month] + j]));
+                    }
+                }
+                Days = Days1;
+            }
+        }
+        try {
+            if (Сommands[i][0] == "DUMP") {
+                if (std::stoi(Сommands[i][1]) > DaysInMonth[next_month]) {
+                    throw std::runtime_error("There is no such number in this month");
+                } else {
+                    std::cout << Days[stoi(Сommands[i][1]) - 1].size() << "  ";
+                    for (int j = 0; j < Days[stoi(Сommands[i][1]) - 1].size(); j++) {
+                        std::cout << Days[stoi(Сommands[i][1]) - 1][j] << "  ";
+                    }
+                    std::cout << std::endl;
+                }
+            }
+        } catch (std::exception& ex) {
+            std::cout << ex.what() << std::endl;
+        }
+        if (Сommands[i][0] == "Wrong command") {
+            std::cout << "Wrong command" << std::endl;
+        }
+    }
+    return 0;
+}
---
+++ synonims.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+
+//https://www.coursera.org/learn/c-plus-plus-white/programming/WRbZE/sinonimy
+/*
+Два слова называются синонимами друг друга, если они имеют похожие значения.
+Реализуйте следующие операции над словарём синонимов:
+
+- ADD word1 word2 — добавить в словарь пару синонимов (word1, word2).
+- COUNT word — узнать количество синонимов слова word.
+- CHECK word1 word2 — проверить, являются ли слова word1 и word2 синонимами.
+Слова word1 и word2 считаются синонимами, если среди запросов ADD
+был хотя бы один запрос ADD word1 word2 или ADD word2 word1.
+
+Формат ввода
+Сначала вводится количество запросов Q, затем Q строк с описаниями запросов.
+Гарантируется, что в каждом запросе CHECK и ADD слова word1 и word2 различны.
+Все слова состоят лишь из латинских букв, цифр и символов подчёркивания.
+
+Формат вывода
+Для каждого запроса в соответствующей строке выведите ответ на него:
+В ответ на запрос COUNT word выведите единственное целое число — количество синонимов слова word.
+В ответ на запрос CHECK word1 word2 выведите строку YES, если word1 и word2 являются синонимами,
+и NO в противном случае.
+*/
+
+int main() {
+    int number_of_commands = 0;
+    std::cout << "Enter the number of commands : ";
+    std::cin >> number_of_commands;
+    std::vector<std::vector<std::string>> Commands(number_of_commands);
+    std::string command, word, word1, word2;
+    std::cout << "Enter commands: " << std::endl;
+    for (int i = 0; i < number_of_commands; i++) {
+        std::cin >> command;
+        if (command == "ADD" || command == "CHECK" || command == "COUNT") {
+            if ((command == "ADD") || (command == "CHECK")) {
+                std::cin >> word1 >> word2;
+                Commands[i].push_back(command);
+                Commands[i].push_back(word1);
+                Commands[i].push_back(word2);
+            }
+            if (command == "COUNT") {
+                std::cin >> word;
+                Commands[i].push_back(command);
+                Commands[i].push_back(word);
+            }
+        } else {
+            Commands[i].push_back("Wrong command");
+        }
+    }
+    std::cout << std::endl;
+    std::map<std::string, std::set<std::string>> SynonymDictionary;
+    for (int i = 0; i < number_of_commands; i++) {
+        if (Commands[i][0] == "ADD") {
+            SynonymDictionary[Commands[i][1]].insert(Commands[i][2]);
+            SynonymDictionary[Commands[i][2]].insert(Commands[i][1]);
+        }
+        if (Commands[i][0] == "COUNT") {
+            std::cout << SynonymDictionary[Commands[i][1]].size() << std::endl;
+        }
+        if (Commands[i][0] == "CHECK") {
+            if (SynonymDictionary[Commands[i][1]].count(Commands[i][2]) == 1) {
+                std::cout << "YES" << std::endl;
+            } else {
+                std::cout << "NO" << std::endl;
+            }
+        }
+        if (Commands[i][0] == "Wrong command") {
+            std::cout << "Wrong command" << std::endl;
+        }
+    }
+    return(0);
+}
